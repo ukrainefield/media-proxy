64b734c7e1e1aaf9a2d70cca6ee408485ee25f3f
Jaap <33700526+GewoonJaap@users.noreply.github.com>
2022-03-03 11:17:28 +0100

    Error handler

--- server.js
+++ server.js
@@ -9,42 +9,48 @@ const PORT = process.env.PORT || 4000;
 app.use(express.static('dist'));
 
 app.get('/proxy/media/*', (req, res) => {
-    const mediaURL = req.params[0]
-    console.log('Proxying media: ', mediaURL);
+    try {
+        const mediaURL = req.params[0]
+        console.log('Proxying media: ', mediaURL);
 
-    let parts = url.parse(mediaURL);
+        let parts = url.parse(mediaURL);
 
-    var filename = parts.pathname.split("/").pop();
+        var filename = parts.pathname.split("/").pop();
 
-    var options = {
-        port: (parts.protocol === "https:" ? 443 : 80),
-        host: parts.hostname,
-        method: 'GET',
-        path: parts.path,
-        accept: '*/*'
-    };
+        var options = {
+            port: (parts.protocol === "https:" ? 443 : 80),
+            host: parts.hostname,
+            method: 'GET',
+            path: parts.path,
+            accept: '*/*'
+        };
 
-    var request = (options.port === 443 ? https.request(options) : http.request(options));
+        var request = (options.port === 443 ? https.request(options) : http.request(options));
 
-    request.addListener('response', function (proxyResponse) {
-        var offset = 0;
-        var contentLength = parseInt(proxyResponse.headers["content-length"], 10);
-        var body = new Buffer(contentLength);
+        request.addListener('response', function (proxyResponse) {
+            var offset = 0;
+            var contentLength = parseInt(proxyResponse.headers["content-length"], 10);
+            var body = new Buffer(contentLength);
 
-        proxyResponse.setEncoding('binary');
-        proxyResponse.addListener('data', function(chunk) {
-            body.write(chunk, offset, "binary");
-            offset += chunk.length;
-        }); 
+            proxyResponse.setEncoding('binary');
+            proxyResponse.addListener('data', function (chunk) {
+                body.write(chunk, offset, "binary");
+                offset += chunk.length;
+            });
 
-        proxyResponse.addListener('end', function() {
-            res.contentType(filename);
-            res.write(body);
-            res.end();            
+            proxyResponse.addListener('end', function () {
+                res.contentType(filename);
+                res.write(body);
+                res.end();
+            });
         });
-    });
 
-    request.end();
+        request.end();
+    } catch (e) {
+        res.status(500).json({
+            error: e.message
+        });
+    }
 });
 
 app.listen(PORT);
