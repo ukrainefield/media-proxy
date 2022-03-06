963124497a88d6accc0a5b0301fac3412a88f456
Jaap <33700526+GewoonJaap@users.noreply.github.com>
2022-03-06 10:54:24 +0100

    Fix media url when corrupt

--- routes/mediaProxy.js
+++ routes/mediaProxy.js
@@ -5,8 +5,9 @@ const axios = require('axios').default;
 /* GET home page. */
 router.get('/*', async function (req, res, next) {
   try {
-    const mediaURL = req.params[0];
+    let mediaURL = req.params[0];
     console.log('Proxying media: ', mediaURL);
+    mediaURL = fixMediaUrl(mediaURL);
 
     axios
       .get(mediaURL, {
@@ -27,5 +28,12 @@ router.get('/*', async function (req, res, next) {
     });
   }
 });
+
+function fixMediaUrl(url) {
+  if (url.startsWith('ams3.digitaloceanspaces.com/ukrainefield-storage/')) {
+    url = url.replace('ams3.digitaloceanspaces.com/ukrainefield-storage/', 'https://ukrainefield-storage.ams3.digitaloceanspaces.com/');
+  }
+  return url;
+}
 
 module.exports = router;
