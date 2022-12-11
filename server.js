ae03505e8e6a7d5a10a4290764b839448887e6e7
Jaap <jaap@mrproper.dev>
2022-12-11 16:13:08 +0100

    Metrics

--- server.js
+++ server.js
@@ -4,7 +4,15 @@ const axios = require('axios').default;
 const PORT = process.env.PORT || 4000;
 const path = require('path');
 const mediaProxyRouter = require('./routes/mediaProxy');
-
+const promBundle = require('express-prom-bundle');
+const metricsMiddleware = promBundle({
+  includeMethod: true,
+  includeStatusCode: true,
+  promClient: {
+    collectDefaultMetrics: {},
+  },
+});
+app.use(metricsMiddleware);
 app.use(express.static('dist'));
 
 app.use('/proxy/media', mediaProxyRouter);
